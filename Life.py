b2d2c047c97cd79d8a68f359417cffd45e168f2e
biswadeepdatta77 <91516122+biswadeepdatta77@users.noreply.github.com>
2021-10-03 15:23:41 +0530

    Add files via upload

---
+++ Life.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age "))
+year = 90-age
+weeks = 52*year
+months = year*12
+days = 365*year
+print("You have "+str(weeks)+" weeks which is "+str(months)+" months which is "+str(days)+" days left")
